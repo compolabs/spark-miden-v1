880ded8bf9399b90fd6877128f62564fd5058c93
riemann <aleqvids@gmail.com>
2024-06-12 13:22:06 -0400

    feat msb

--- tests/mock_integration/swap_math_test.rs
+++ tests/mock_integration/swap_math_test.rs
@@ -190,12 +190,43 @@ pub fn test_fixed_point_div() {
     let assembler = TransactionKernel::assembler().with_debug_mode(true);
 
     // Values to be formatted
-    let amount_a = format_value_with_decimals(1, 8);
-    let amount_b = format_value_with_decimals(70000, 8);
+    let amount_a = format_value_with_decimals(70000, 8);
+    let amount_b = format_value_with_decimals(101, 8);
 
     let assembly_code = format!(
         "
       use.std::math::u64
+
+      # input: [u64 a_hi, u64 a_lo]
+      proc.msb
+        mem_store.10
+        mem_store.11
+
+        push.0  
+        mem_store.20
+
+        push.1
+
+        while.true
+          mem_load.10
+          mem_load.11
+
+          exec.u64::shr
+
+          mem_store.10
+          mem_store.11
+
+          mem_load.20
+          push.1
+          add 
+          mem_store.20
+        end
+
+        mem_load.20
+        push.1
+        sub
+
+      end
 
       begin
         push.{amount_a}
@@ -208,9 +239,47 @@ pub fn test_fixed_point_div() {
         mem_store.4
         u32split
 
+        push.222
+        debug.stack
+        drop
+
+        exec.u64::gt
+
+        push.111
+        debug.stack
+        drop
+
+        if.true
+
+          push.18446744069414584320
+          u32split
+          mem_load.3
+          u32split
+
+          push.222
+          debug.stack
+          drop
+          exec.u64::div
+
+    
+
+          push.333
+          debug.stack
+          drop
+        else
+
+        ##### CONTINUE 
+
+
+
+        end
+
         exec.u64::div
 
         push.4294967296 mul add
+
+        # if a < b, scale by 1e5
+        debug.stack
 
         # save result to mem address 0
         mem_store.0
@@ -246,8 +315,8 @@ pub fn test_fixed_point_div() {
         push.1000
         lt
         if.true
-          # scale 1e8
-          push.100000000
+          # scale 1e6
+          push.1000000
           mem_store.1
         else end
 
@@ -283,17 +352,24 @@ pub fn test_fixed_point_div() {
         debug.mem
 
         mem_load.1
-        mem_load.3
-
-        mul
-
         u32split
+
+        mem_load.3
+        u32split
+
+
+        # END
+
+        exec.u64::wrapping_mul
 
         mem_load.4
         u32split
 
         exec.u64::div
-        
+
+        push.4294967296 mul add
+
+        debug.stack
 
       end
     ",
@@ -337,6 +413,100 @@ pub fn test_fixed_point_div() {
         rust_result,
         margin
     ); */
+}
+
+#[test]
+pub fn test_msb_masm() {
+    // Instantiate the assembler
+    let assembler = TransactionKernel::assembler().with_debug_mode(true);
+
+    // Values to be formatted
+    let amount_a = format_value_with_decimals(1, 8);
+
+    let assembly_code = format!(
+        "
+        use.std::math::u64
+
+        # input: [u64 a_hi, u64 a_lo]
+        proc.msb
+          mem_store.10
+          mem_store.11
+  
+          push.0
+          mem_store.20
+  
+          push.1
+
+          while.true
+            mem_load.11
+            mem_load.10
+
+            push.1
+
+            exec.u64::shr
+
+            mem_store.10
+            mem_store.11
+  
+            mem_load.20
+            push.1
+            add 
+            mem_store.20
+
+            mem_load.11
+            push.0
+            mem_load.10
+            push.0
+
+            neq neq neq
+
+            if.true
+              push.1
+            else
+              push.0
+            end
+
+          end
+  
+          mem_load.20
+          push.1
+          sub
+  
+        end
+
+        begin
+          push.{amount_a}
+          u32split
+
+          exec.msb
+          
+          push.111
+          debug.stack
+          drop
+        end
+    ",
+        amount_a = amount_a,
+    );
+
+    // Compile the program from the loaded assembly code
+    let program = assembler
+        .compile(assembly_code)
+        .expect("Failed to compile the assembly code");
+
+    let stack_inputs = StackInputs::try_from_ints([]).unwrap();
+
+    let host = DefaultHost::default();
+
+    // Execute the program and generate a STARK proof
+    let (outputs, _proof) = prove(&program, stack_inputs, host, ProvingOptions::default())
+        .expect("Failed to execute the program and generate a proof");
+
+    println!("outputs: {:?}", outputs);
+
+    // Get the result from the assembly output and convert to i64
+    let assembly_result_u64: u64 = outputs.stack()[0].into();
+    let assembly_result: i64 = assembly_result_u64 as i64;
+    println!("assembly_result: {}", assembly_result);
 }
 
 #[test]
