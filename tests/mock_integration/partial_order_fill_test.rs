f193ece9f8e6a36d1dd9299142f38d0028be2da4
partylikeits1983 <aleqvids@gmail.com>
2024-07-18 22:51:38 +0300

    feat: add invariate test

--- tests/mock_integration/partial_order_fill_test.rs
+++ tests/mock_integration/partial_order_fill_test.rs
@@ -1161,6 +1161,110 @@ fn test_swap_zero_amount() {
     assert!(tx_result.is_err());
 }
 
+// @dev Test swapping amount invalid note args amount
+#[test]
+fn test_swap_false_amount_via_note_args() {
+    // ASSETS
+    // Offered Asset
+    let faucet_id_1 = AccountId::try_from(ACCOUNT_ID_FUNGIBLE_FAUCET_ON_CHAIN).unwrap();
+    let amount_token_a: u64 = format_value_with_decimals(607, 6);
+    let offered_token_a: Asset = FungibleAsset::new(faucet_id_1, amount_token_a)
+        .unwrap()
+        .into();
+
+    // Requested Asset
+    let faucet_id_2 = AccountId::try_from(ACCOUNT_ID_FUNGIBLE_FAUCET_ON_CHAIN_1).unwrap();
+    let requested_amount_token_b = format_value_with_decimals(987, 6);
+    let requested_token_b: Asset = FungibleAsset::new(faucet_id_2, requested_amount_token_b)
+        .unwrap()
+        .into();
+
+    // ACCOUNT IDs
+    // SWAPp note creator
+    let swapp_creator_account_id = AccountId::try_from(ACCOUNT_ID_SENDER).unwrap();
+
+    // SWAPp note consumer
+    let swapp_consumer_account_id = AccountId::try_from(ACCOUNT_ID_SENDER_1).unwrap();
+
+    // SWAPp note consumer wallet balance
+    let swap_consumer_balance_token_b = format_value_with_decimals(0, 0);
+    let swap_consumer_token_b = FungibleAsset::new(faucet_id_2, swap_consumer_balance_token_b)
+        .unwrap()
+        .into();
+    let (target_pub_key, target_falcon_auth) = get_new_pk_and_authenticator();
+
+    // SWAPp note consumer wallet
+    let swap_consumer_wallet = get_custom_account_code(
+        swapp_consumer_account_id,
+        target_pub_key,
+        Some(swap_consumer_token_b),
+    );
+
+    // SWAPp note
+    let (swap_note, _payback_note, _note_script_hash) = create_partial_swap_note(
+        swapp_creator_account_id.clone(),
+        swapp_creator_account_id.clone(),
+        offered_token_a,
+        requested_token_b,
+        NoteType::OffChain,
+        [Felt::new(1), Felt::new(2), Felt::new(3), Felt::new(4)],
+    )
+    .unwrap();
+
+    // CONSTRUCT AND EXECUTE TX (Success)
+    // --------------------------------------------------------------------------------------------
+    let tx_context = TransactionContextBuilder::new(swap_consumer_wallet.clone())
+        .input_notes(vec![swap_note.clone()])
+        .build();
+
+    let mut executor =
+        TransactionExecutor::new(tx_context.clone(), Some(target_falcon_auth.clone()))
+            .with_debug_mode(true);
+    executor.load_account(swapp_consumer_account_id).unwrap();
+
+    let block_ref = tx_context.tx_inputs().block_header().block_num();
+    let note_ids = tx_context
+        .tx_inputs()
+        .input_notes()
+        .iter()
+        .map(|note| note.id())
+        .collect::<Vec<_>>();
+
+    let tx_script_code = include_str!("../../src/tx_scripts/tx_script.masm");
+    let tx_script_ast = ProgramAst::parse(tx_script_code).unwrap();
+
+    let invalid_note_args_amount= format_value_with_decimals(100, 6);
+
+    // amount to consume
+    let note_args = [[
+        Felt::new(invalid_note_args_amount),
+        Felt::new(0),
+        Felt::new(0),
+        Felt::new(0),
+    ]];
+
+    let note_args_map = BTreeMap::from([(note_ids[0], note_args[0])]);
+
+    let tx_script_target = executor
+        .compile_tx_script(tx_script_ast.clone(), vec![], vec![])
+        .unwrap();
+
+    let tx_args_target = TransactionArgs::new(
+        Some(tx_script_target),
+        Some(note_args_map),
+        AdviceMap::default());
+
+    // Execute the transaction and get the witness
+    let tx_result = executor.execute_transaction(
+        swapp_consumer_account_id,
+        block_ref,
+        &note_ids,
+        tx_args_target.clone(),
+    );
+
+    assert!(tx_result.is_err());
+}
+
 // @dev Test that a SWAPp note consumer can specify the amount to consume via note args
 #[test]
 fn test_partial_swap_fill_with_note_args() {
